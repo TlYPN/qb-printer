ae063d02263bf42cd8d7e50867f8535456861fe0
Anomaly Roleplay <92613645+TlYPN@users.noreply.github.com>
2023-04-29 12:36:13 -0400

    added more printers to config

--- qb-printer/config.lua
+++ qb-printer/config.lua
@@ -3,5 +3,7 @@ Config = {}
 Config.UseTarget = GetConvar('UseTarget', 'true') == 'true'
 
 Config.printers = {
-    'prop_printer_01'
+    'prop_printer_01',
+    'v_res_printer',
+    'prop_printer_02,
 }
